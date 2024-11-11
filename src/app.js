ae67ac2949176ea14e448b7a4a346c0f5b6e8a45
Alex Bradau <alexbradau@icloud.com>
2024-11-10 22:36:16 -0500

    now making openai api call

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const generationController = require('./controller/generationController')
 
 const app = express();
 
-app.use(express.json());
+app.use(express.raw({ type: '*/*', limit: '10mb' }));
 app.use(generationController);
 
 const port = process.env.PORT || 5000;
--- src/controller/generationController.js
+++ src/controller/generationController.js
@@ -4,8 +4,18 @@ const controller = new express.Router()
 
 controller.post('/generateCode', async (req, res) => {
     try {
-        console.log('Exposed endpoint...')
-        res.send('Exposed endpoint...')
+        console.log(req.body)
+        const completion = await openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+                { role: "system", content: "You are a coding assistant that generates java classes from their associated test classes." },
+                {
+                    role: "user",
+                    content: "Generate a java class from the following test class and download it as a java class: \n" + req.body,
+                },
+            ],
+        });
+        res.send(completion.choices)
     } catch (e) {
         console.log(e)
     }
