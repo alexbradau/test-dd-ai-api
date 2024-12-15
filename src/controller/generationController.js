b27bd7dcf5d02a07f84c87de0d6be485eeff52f3
Alex Bradau <alexbradau@icloud.com>
2024-12-15 17:04:53 -0500

    sending output as json and optimised ai

--- src/controller/generationController.js
+++ src/controller/generationController.js
@@ -7,15 +7,17 @@ controller.post('/generateCode', async (req, res) => {
         console.log(req.body)
         const completion = await openai.chat.completions.create({
             model: "gpt-4o",
+            response_format : {"type": "json_object"},
             messages: [
-                { role: "system", content: "You are a coding assistant that generates java classes from their associated test classes." },
+                { role: "system", content: "You are a coding assistant that generates java classes from their associated test classes. Return the response in a json format with the prose under its own variables and the actual outputted code under its own variable. Do not include line breaker '\n' in output code." },
                 {
                     role: "user",
                     content: "Generate a java class from the following test class and download it as a java class: \n" + req.body,
                 },
             ],
         });
-        res.send(completion.choices)
+        var output = JSON.parse(completion.choices[0].message.content)
+        res.send(output)
     } catch (e) {
         console.log(e)
     }
