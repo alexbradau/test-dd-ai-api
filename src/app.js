27e9e02a7566810d53cf718c4e48baa895f4d09e
Alex Bradau <alexbradau@icloud.com>
2024-12-21 13:31:51 -0500

    files

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const generationController = require('./controller/generationController')
+const express = require("express");
+const bodyParser = require("body-parser");
+const generationController = require("./controller/generationController");
 
 const app = express();
 
-app.use(express.raw({ type: '*/*', limit: '10mb' }));
+app.use(express.raw({ type: "*/*", limit: "10mb" }));
 app.use(generationController);
 
 const port = process.env.PORT || 5000;
