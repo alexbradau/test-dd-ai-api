acc25056679074486305e383544a8fd23b6755f9
Alex Bradau <alexbradau@gmail.com>
2025-01-04 22:15:08 -0500

    changed port to 6000

--- src/app.js
+++ src/app.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.raw({ type: "*/*", limit: "10mb" }));
 app.use(generationController);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 app.listen(port);
